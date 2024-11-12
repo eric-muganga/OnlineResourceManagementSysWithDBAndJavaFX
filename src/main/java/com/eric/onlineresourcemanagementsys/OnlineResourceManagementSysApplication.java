e6503371a62e6a05020e71f2149c79a640f737cf
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2024-11-12 13:02:50 +0100

    initial

---
+++ src/main/java/com/eric/onlineresourcemanagementsys/OnlineResourceManagementSysApplication.java
@@ -0,0 +1,13 @@
+package com.eric.onlineresourcemanagementsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineResourceManagementSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineResourceManagementSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eric/onlineresourcemanagementsys/OnlineResourceManagementSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric.onlineresourcemanagementsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineResourceManagementSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
