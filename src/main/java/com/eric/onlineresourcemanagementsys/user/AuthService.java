c548774ba184aa294f64587869d525441834bbb8
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2024-11-21 03:36:02 +0100

    feat:Refactored the user register and login methods

--- src/main/java/com/eric/onlineresourcemanagementsys/user/AuthService.java
+++ src/main/java/com/eric/onlineresourcemanagementsys/user/AuthService.java
@@ -15,24 +15,27 @@ public class AuthService {
 
     public void registerUser(String username, String password) {
         // Check if the user already exists in the file
-        if (FileHandler.loadUserFromFile(username) == null) {
-            // Creating a new user and save directly to file
-            User newUser = new User(username, password, secretKey);
-            FileHandler.saveUserToFile(newUser); // Save user to persistent storage
-            FileHandler.saveUserResourcesToFile(newUser); // Save empty resources
-            System.out.println("Registration successful");
-        } else {
-            System.out.println("Username already exists.");
+        if (FileHandler.loadUserFromFile(username) != null) {
+            throw new IllegalArgumentException("User already exists");
         }
+        // Creating a new user and save directly to file
+        User newUser = new User(username, password, secretKey);
+        FileHandler.saveUserToFile(newUser);// Saving user to persistent storage
+        FileHandler.saveUserResourcesToFile(newUser); // Saving empty resources
+        System.out.println("Registration successful");
     }
 
     public User loginUser(String username, String password) {
         // Loading user data from file each time
         User user = FileHandler.loadUserFromFile(username);
-
         if (user != null && user.verifyPassword(password, secretKey)) {
             // Loading the user's resources from file after successful login
-            user.getResourceManager().setResources(FileHandler.loadUserResourcesFromFile(user));
+            var resources = FileHandler.loadUserResourcesFromFile(user);
+            if (resources != null) {
+                user.getResourceManager().setResources(resources);
+            } else {
+                System.out.println("No resources found for user: " + username);
+            }
             return user;
         } else {
             System.out.println("Invalid username or password.");
